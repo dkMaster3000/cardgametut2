c3c4c13dab1ff0e67077e17a34947680ac9261a2
dkMaster3000 <kunzd007@gmail.com>
2023-10-30 14:13:02 +0100

    shuffle deck

--- cardgametut2/Assets/PlayerDeck.cs
+++ cardgametut2/Assets/PlayerDeck.cs
@@ -6,6 +6,7 @@ public class PlayerDeck : MonoBehaviour
 {
 
     public List<Card> deck = new List<Card>();
+    public List<Card> container = new List<Card>();
 
     public int x;
     public int deckSize;
@@ -18,7 +19,7 @@ public class PlayerDeck : MonoBehaviour
 
         for(int i = 0; i < deckSize; i++)
         {
-            x = Random.Range(1, 4);
+            x = Random.Range(1, 5);
             deck[i] = CardDataBase.cardList[x];
         }
     }
@@ -27,5 +28,16 @@ public class PlayerDeck : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Shuffle()
+    {
+        for(int i = 0; i < deckSize;i++)
+        {
+            container[0] = deck[i];
+            int randomIndex = Random.Range(i, deckSize);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = container[0];
+        }
     }
 }
