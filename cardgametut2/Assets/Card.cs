69ae2b278e716c354500db25aef1f4e8dfa2b16a
dkMaster3000 <kunzd007@gmail.com>
2023-10-30 11:56:51 +0100

    initialise

---
+++ cardgametut2/Assets/Card.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class Card : MonoBehaviour
+{
+    public int id;
+    public string cardName;
+    public int cost;
+    public int Power;
+    public string cardDescription;
+
+    public Card()
+    {
+
+    }
+}
